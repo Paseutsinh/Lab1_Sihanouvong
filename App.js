4f75e372c7bfe875a36e5126a3d5a9efc63a10bb
Paseutsinh <2024802010529@student.tdmu.edu.vn>
2024-04-14 10:54:10 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Helloworld from './src/Helloworld';
+import Capturingtap from './src/Capturingtap';
+import Customcomponent from './src/Customcomponent';
+import Stateprop from './src/Stateprop';
+import Styling from './src/Styling';
+import Scrollablecotent from './src/Scrollablecotent';
+import Buildingaform from './src/Buildingaform';
+import Longlists from './src/Longlists';
+export default function App() {
+  return (
+      <Longlists/>
+  );
+}
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+Sihanouvong Paseutsinh
